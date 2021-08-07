618d3d5d39828b318fd3ddf4fa7c7309cfbb0959
Edwin Kofler <edwin@kofler.dev>
2021-08-06 18:42:57 -0700

    fix: Revert shopt options at end of script

--- pkg/lib/cmd/bash-toml.sh
+++ pkg/lib/cmd/bash-toml.sh
@@ -1,8 +1,5 @@
 # shellcheck shell=bash
 
-# TODOD: ensure shopt options are set back / remove set
-set -ETeo pipefail
-shopt -s nullglob extglob globasciiranges
 if [ -n "${DEBUG+x}" ]; then
 	trap 'bash_toml.trap_error' 'ERR'
 	bash_toml.trap_error() {
@@ -16,6 +13,21 @@ for f in "$BASH_TOML_LIB_DIR"/util/?*.sh; do
 done
 
 bash-toml() {
+	# shellcheck disable=SC1007
+	local setPipefail= setGlobasciiranges=
+
+	if [ -o pipefail ]; then
+		setPipefail='yes'
+	else
+		setPipefail='no'
+	fi
+
+	if shopt -q globasciiranges; then
+		setGlobasciiranges='yes'
+	else
+		setGlobasciiranges='no'
+	fi
+
 	for arg; do
 		case "$arg" in
 		stdin)
@@ -29,4 +41,16 @@ bash-toml() {
 			;;
 		esac
 	done
+
+	if [ "$setPipefail" ]; then
+		set -o pipefail
+	else
+		set +o pipefail
+	fi
+
+	if [ "$setGlobasciiranges" ]; then
+		shopt -s globasciiranges
+	else
+		shopt -u globasciiranges
+	fi
 }
